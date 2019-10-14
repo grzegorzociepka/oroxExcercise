a0f93954ff9029b29183f95081546ba609486c6b
Grzesiek <grzegorzociepka09@gmail.com>
2019-10-14 21:30:59 +0200

    Add backend

---
+++ oroxExcerciseBackend/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace oroxExcerciseBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<int> Get()
+        {
+            Random random = new Random();
+            return random.Next();
+        }
+    }
+}
