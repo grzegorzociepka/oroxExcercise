b4750f89018b927068ecee05f4e096fd53aaac11
Grzesiek <grzegorzociepka09@gmail.com>
2019-10-14 23:32:22 +0200

    added logic with update every sec

--- front/oroxExcerciseFront/src/app/components/numbers/numbers.component.ts
+++ front/oroxExcerciseFront/src/app/components/numbers/numbers.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { NumberService } from 'src/app/services/number.service';
+import { Observable, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
   styleUrls: ['./numbers.component.css']
 })
-export class NumbersComponent {
+export class NumbersComponent implements OnInit{
 
   ActualNumber = 0;
-
-  constructor(private numberService:NumberService) { }
   
-  async getRandomNumber(){
-    this.ActualNumber = await this.numberService.getNumber();
+  ngOnInit(){
+    const numbers = timer(1000,1000);
+    numbers.subscribe(x => this.ActualNumber++);
+  }
+
+  constructor(private numberService: NumberService) { }
+
+  async getRandomNumber() {
+
+    const randomNumber = await this.numberService.getNumber();
+    const dt = new Date();
+    const secs = dt.getSeconds() + (60 * (dt.getMinutes() + (60 * dt.getHours())));
+    this.ActualNumber = randomNumber + secs;
   }
 }
