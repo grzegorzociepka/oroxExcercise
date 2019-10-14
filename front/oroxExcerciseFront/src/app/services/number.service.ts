fe490f8645822710d202887e848a0380484376bc
Grzesiek <grzegorzociepka09@gmail.com>
2019-10-14 23:34:29 +0200

    remove unused import

--- front/oroxExcerciseFront/src/app/services/number.service.ts
+++ front/oroxExcerciseFront/src/app/services/number.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 const BASE_URL = "https://localhost:5001/api/values";
